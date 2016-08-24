38fcc510dc4411f8c5edf66519ffaa5d8166aae5
tiano1996 <circle4tiano@gmail.com>
2016-08-24 17:56:27 +0800

    fixed修改密码

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -38,31 +38,19 @@ UserSchema.pre('save', function(next){
 
   if (this.isNew) {
     this.meta.createAt = this.meta.updateAt = Date.now();
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-      if(err) return next(err);
-
-      bcrypt.hash(user.password, salt, function(err, hash) {
-        if(err) return next(err);
-
-        user.password = hash;
-        next();
-      });
-    });
   } else {
     this.meta.updateAt = Date.now();
-    console.log(this);
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
       if(err) return next(err);
 
-      bcrypt.hash(user.password, salt, function(err, hash) {
-        if(err) return next(err);
-
-        user.password = hash;
-        next();
-      });
+      user.password = hash;
+      next();
     });
-
-  }
+  });
 
 
 });
